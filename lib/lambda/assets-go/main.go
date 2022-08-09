7160d4155e580c34a4bea008418948e95c918f93
JoeNonExotic <vijayvikramreddy@gmail.com>
2022-08-09 13:33:22 -0700

    minor updates to .gitignore and adding docs to main.go

--- lib/lambda/assets-go/main.go
+++ lib/lambda/assets-go/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 )
 
+// HandleRequest Handles incoming event with a passed in ctx and req.
 func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println(fmt.Sprintf("Received request %v with context %s", req, ctx))
 	return events.APIGatewayProxyResponse{
@@ -16,6 +17,7 @@ func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (even
 	}, nil
 }
 
+// main entry point for lambda.
 func main() {
 	lambda.Start(HandleRequest)
 }
